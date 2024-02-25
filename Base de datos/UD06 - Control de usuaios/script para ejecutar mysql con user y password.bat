dd77d724799ae87a77c0fb507ec01c232ae3f2a7
marcos <mremonb@iesch.org>
2024-02-26 00:29:22 +0100

    a

--- Base de datos/UD06 - Control de usuaios/script para ejecutar mysql con user y password.bat
+++
@@ -1,4 +0,0 @@
-set/p nombre=¿Nombre de usuario?
-set/p password=¿Password?
-"C:\Program Files\MySQL\MySQL Server 8.1\bin\mysql" -u  %nombre%  -p%password%
-pause
--- Programacion/UD05 - POO (Python)/Ejercicio de POO (multiobjeto)/Tienda/Cliente.py
+++
@@ -1,9 +0,0 @@
-class Cliente:
-
-    def __init__(self):
-        self.lista_compra = []
-        self.gasto = 0.0
-
-    def compra_add(self, articulo):
-        self.lista_compra.append(articulo)
-        self.gasto += articulo.precio
