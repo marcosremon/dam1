2dc5bec944eebe32aff30b5a60be7b91220ab4ad
marcos <mremonb@iesch.org>
2024-05-02 21:09:22 +0200

    2-5-2024

---
+++ Entornos de desarrollos/UD5 - Análisis y POO Diagramas De Clases/02Granja/src/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public abstract class Animal {
+    
+    protected int nPatas;
+    
+    public abstract void hazRuido();
+    
+}
