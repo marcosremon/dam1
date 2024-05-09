46831f8a5cc5dd45a25186f2a871842654143b66
marcosremon <mremonb@iesch.org>
2024-05-09 19:30:37 +0200

    9-5-2024

---
+++ Lenguaje de marcas/UD08 - Vue 3 js/componentes-1/src/components/BlogPost.vue
@@ -0,0 +1,20 @@
+<script setup>
+    //defineProps(['titulo', 'body', 'id', 'colorText']);
+    defineProps({
+        titulo: String,
+        id: Number,
+        body: {
+            type: String,
+            default: "Sin descripcion",
+        }
+    })
+</script>
+<template>
+    <div class="card">
+        <div class="card-body">
+            <!-- <h5 class="card-title" :class="`text-${colorText}`"> {{ id }} - {{ titulo }}</h5> -->
+            <h5> {{ id }} - {{ titulo }}</h5>
+            <p>{{ body }}</p>
+        </div>
+    </div>
+</template>
---
+++ Lenguaje de marcas/UD08 - Vue 3 js/componentes-1/src/components/ButtonCounter.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue';
+
+
+    const contador = ref(0);
+    const incrementar = () => contador.value++;
+</script>
+<template>
+    <button @click="incrementar">contador - {{ contador }}</button>
+</template>
