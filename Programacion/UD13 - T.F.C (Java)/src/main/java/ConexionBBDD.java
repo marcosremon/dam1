ca0061e2d73b0e9f982e44a316f4af0d4edd9bf1
marcos <mremonb@iesch.org>
2024-05-13 13:39:31 +0200

    13-5-2024

---
+++ Programacion/UD13 - T.F.C (Java)/src/main/java/ConexionBBDD.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBBDD {
+    private static final String URL = "jdbc:mysql://localhost:3306/banco";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
